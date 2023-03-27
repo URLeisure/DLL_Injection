3ea310dd0d34ee0090ba72d7b9e5836cd7c3bdbc
URLeisure <LChenSED@163.COM>
2023-03-27 16:16:06 +0800

    Add files via upload

---
+++ Dll1/Dll1/dllmain.cpp
@@ -0,0 +1,23 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include "stdlib.h"
+#include <iostream>
+
+using namespace std;
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+
+{
+	switch (ul_reason_for_call) {
+
+	case DLL_PROCESS_ATTACH: {
+		// 当DLL被进程 <<第一次>> 调用时，导致DllMain函数被调用，
+		HWND hwnd = GetActiveWindow();
+		MessageBox(hwnd, L"DLL已进入目标进程。", L"信息", MB_ICONINFORMATION); //弹出一个模态对话框
+	}
+	}
+	return TRUE;
+}
